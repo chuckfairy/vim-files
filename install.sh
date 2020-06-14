b5b2a6ba95dae298e821cfc323c24f2fb7a7370f
chuckfairy <charlieabeling@gmail.com>
2020-06-14 14:04:14 -0500

    ycm and other install updates

--- install.sh
+++ install.sh
@@ -3,3 +3,19 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s ~/.vim/.vimrc ~/.vimrc;
+
+
+echo "Installing ctags and checking vim"
+
+sudo apt install vim ctags
+
+
+echo "Installing vim Plugins"
+
+vim -c :PluginInstall -c :qa
+
+
+echo "Install YCM modules"
+
+#cd ./bundle/YouCompleteMe;
+./bundle/YouCompleteMe/install.py --clangd-completer --ts-completer --cs-completer
